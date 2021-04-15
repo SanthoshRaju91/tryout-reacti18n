2350637a7151b88888c8161f2f3cdba292586810
Nagaraj Santhosh <santhosh.nagaraj@rakuten.com>
2021-04-15 12:08:07 +0530

    Fixing test case warning for reacti18next

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { I18nextProvider } from "react-i18next";
 import App from "./App";
+import i18n from "./i18n";
 
 test("renders learn react link", () => {
   renderApp();
@@ -9,7 +11,11 @@ test("renders learn react link", () => {
 });
 
 function renderApp() {
-  const renderOptions = render(<App />);
+  const renderOptions = render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  );
   return {
     ...renderOptions,
   };
